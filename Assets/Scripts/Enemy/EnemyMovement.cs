c8bbb4e31749c36529b0fb4fa50ca5c5996ac42c
kuzmin6677 <33939131+kuzmin6677@users.noreply.github.com>
2017-12-13 16:02:43 +0500

    fixed MeshAgent
    1212

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
 
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player").transform;
-        playerHealth = player.GetComponent <PlayerHealth> ();
-        enemyHealth = GetComponent <EnemyHealth> ();
+        //playerHealth = player.GetComponent <PlayerHealth> ();
+        //enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
     }
 
@@ -21,16 +21,16 @@ public class EnemyMovement : MonoBehaviour
     void Update ()
     {
         // If the enemy and the player have health left...
-        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        {
+        //if (enemyHealth.currentHealth > 0 /* && playerHealth.currentHealth > 0*/)
+        //{
             // ... set the destination of the nav mesh agent to the player.
             nav.SetDestination(player.position);
-        }
+        //}
         // Otherwise...
-        else
-        {
+        //else
+        // {
             // ... disable the nav mesh agent.
-            nav.enabled = false;
-        }
+           // nav.enabled = false;
+        //}
     }
 }
